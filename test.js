1f0cd17760580736635e18e3a9455a7329a08c98
OnigiriJack <jackmihalik@icloud.com>
2021-02-11 11:04:42 +0900

    passing string input tests

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const alpha = require("./index");
 
 describe("alpha detector", () => {
     it("should only accept strings", () => {
-        let bin = alpha.toBinary("Jack");
-        expect(bin).to.equal("Buffer <4A, 61, 63, 6B>")
+        let input = "jack"
+        expect(typeof input).to.equal("string")
     })
 })
